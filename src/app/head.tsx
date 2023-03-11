6978bae479261a24c6d0ff69e1572eb4f59f652a
dxtobi <akanbijosephtobi@gmail>
2023-03-10 17:09:02 -0800

    first commit

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -4,10 +4,10 @@ export default function Head() {
     <>
       <title>Beast Games</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="tittle" content="Beast Games"/>
-      <meta property="og:description" content="Best Soccer Game tips"></meta>
+      <meta name="tittle" content="Beast Games, Beast VIP, Beast Telegram"/>
+      <meta property="og:description" content="Best Soccer Game tips, Professional Game Analysis 1000% sure ods."></meta>
       <meta name="description" content="Professional Game Analysis 1000% sure ods." />
-      <meta property="og:site_name" content="Beast Odds"></meta>
+      <meta property="og:site_name" content="Beast Games VIP Odds"></meta>
     </>
   )
 }
